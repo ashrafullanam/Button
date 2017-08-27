aa24a68606afdfcd399c17010486d67aa5797344
ashrafull <ashrafullanam95@gmail.com>
2017-08-27 10:41:59 +0600

    implement Method

---
+++ app/src/main/java/com/example/ashraf/button/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.ashraf.button;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
